e9d4cdb801a1bc6dd1312a20414135c734ea96b1
JUANDA <personalisado2018@gmail.com>
2023-02-08 23:48:42 -0500

    prueba y error

--- prueba.kt
+++ prueba.kt
@@ -1,16 +1,21 @@
-// Ejemplo 1: Ingrese la edad de una persona y valide si es mayor o menor de edad, adicionalmente si
-//  la persona tiene 25 años menciónelo.
-
-println("Ingrese su edad")
-
-val age = readline()!!.toInt();
-
-if (age <18){
-println("usted es menor de edad")
-}else if (age >= 18) {
-    println("usted es mayor de edad")
-}else if (age === 18) {
-    println("usted tiene 25 años, señor $age")
-}else{
-    println("ingrese porfavor un numero")
-}
+// Ejemplo 2:haga un algoritmo que calcule el promedio de 3 números  y si el promedio es mayor a 3.5 indique que gana la materia, si el promedio es mayor a 2 y menor o igual a 3.5 entonces indique que puede recuperar
+fun main(){
+    println("ingrese su primera nota: ");
+    var Nota1 = readLine()!!.toDouble();
+    
+    println("ingrese su segunda nota: ");
+    var Nota2 = readLine()!!.toDouble();
+    
+    println("ingrese su tercera nota: ");
+    var Nota3 = readLine()!!.toDouble();
+    
+    var promedio = ((Nota1+Nota2+Nota3)/3)
+    
+    if((promedio > 2) and (promedio <= 3.5)){
+    println("su promedio fue de $promedio puede recuperar")}
+    else if (promedio > 3.5){
+    println("su promedio fue de $promedio ganaste la materia")}
+    else{
+        println("ingrese un dato valido por favor")
+    }
+    }
